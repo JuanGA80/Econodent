a5740f4ae938af49f82ddc58b97d82b5904048fe
Leonel <leonelvalentinn@gmail.com>
2024-01-10 18:43:49 -0600

    first commit

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+const abrir = document.getElementById('open')
+const cerrar = document.getElementById('close')
+const containerOverlay = document.getElementById('container-overlay')
+
+abrir.addEventListener('click', () => {
+  containerOverlay.style.animation = 'showMenu 0.5s ease-in forwards'
+  containerOverlay.style.display = 'flex'
+})
+cerrar.addEventListener('click', () => {
+  containerOverlay.style.animation = 'closeMenu 0.5s ease-in forwards'
+  setTimeout(() => {
+    containerOverlay.style.display = 'none'
+  }, '500')
+})
