f45ec63fc9cf69acfe50d33e9d32fa184979cf71
Leonel <leonelvalentinn@gmail.com>
2024-01-15 18:54:08 -0600

    Script Menu

--- js/menu.js
+++ js/menu.js
@@ -12,3 +12,9 @@ cerrar.addEventListener('click', () => {
     containerOverlay.style.display = 'none'
   }, '500')
 })
+containerOverlay.addEventListener('click', () => {
+  containerOverlay.style.animation = 'closeMenu 0.5s ease-in forwards'
+  setTimeout(() => {
+    containerOverlay.style.display = 'none'
+  }, '500')
+})
