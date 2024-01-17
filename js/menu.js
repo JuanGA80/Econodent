8a56145683939a06899c991b704ad048576f2ca6
Leonel <leonelvalentinn@gmail.com>
2024-01-17 14:58:55 -0600

    Promotion Script

--- js/menu.js
+++ js/menu.js
@@ -19,3 +19,11 @@ containerOverlay.addEventListener('click', () => {
     containerOverlay.style.display = 'none'
   }, '500')
 })
+
+const menucon = document.getElementById('menu-contacto')
+const whats = document.getElementById('whats')
+const tel = document.getElementById('tel')
+menucon.addEventListener('click', function () {
+  whats.classList.toggle('show-whats')
+  tel.classList.toggle('show-tel')
+})
